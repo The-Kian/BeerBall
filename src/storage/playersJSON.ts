092844da27c2c48986d6501193d630a8554013bb
The-Kian <kian.popat@gmail.com>
2024-04-18 11:56:19 +0100

    UpdateTeams

--- src/storage/playersJSON.ts
+++ src/storage/playersJSON.ts
@@ -1,52 +1,76 @@
 const playersJSON =
-    [
-        {
-            "id": "7e150fd7-db5d-4a86-8f13-dd5e8469dc6a",
-            "playerName": "Josh"
-        },
-        {
-            "id": "283c04e1-2a8a-410a-9a55-4d0dcae2d541",
-            "playerName": "Molly"
-        },
-        {
-            "id": "1dc6078c-6488-40db-b74e-2f91eb2202b0",
-            "playerName": "Audge"
-        },
-        {
-            "id": "1bf8af28-4ef8-4c0f-b969-66756b052da9",
-            "playerName": "Ezra"
-        },
-        {
-            "id": "1dc6078c-6488-40db-3333-2f91eb2202b0",
-            "playerName": "Chris"
-        },
-        {
-            "id": "1bf8af28-4ef8-4c0f-b969-66756b052da9",
-            "playerName": "Ella"
-        },
-        {
-            "id": "1dc6078c-1111-40db-3333-2f91eb2202b0",
-            "playerName": "Sarah"
-        },
-        {
-            "id": "1bf8af28-4ef8-4c0f-9999-66756b052da9",
-            "playerName": "Nathan"
-        },
-        {
-            "id": "1dc6078c-6488-40db-3333-2f91eb2202b0",
-            "playerName": "Wham Sam"
-        },
-        {
-            "id": "1bf8af28-4ef8-4c0f-4421-66756b052da9",
-            "playerName": "Cooper"
-        },
-        {
-            "id": "1dc6078c-6488-40db-3333-2f91eb2202b0",
-            "playerName": "Wham Sam"
-        },
-        {
-            "id": "1bf8af28-4ef8-4c0f-4421-66756b052da9",
-            "playerName": "Cooper"
-        }
-    ];
+[
+    {
+        "id": "1dc6078c-6488-40db-3333-2f91eb2202b0",
+        "playerName": "Chris"
+    },
+    {
+        "id": "1bf8af28-4ef8-4c0f-4421-66756b052da9",
+        "playerName": "Cooper"
+    },
+    {
+        "id": "1bf8af28-4ef8-4c0f-b969-66756b052da9",
+        "playerName": "Ezra"
+    },
+    {
+        "id": "1dc6078c-6488-40db-b74e-2f91eb2202b0",
+        "playerName": "Audge"
+    },
+    {
+        "id": "1dc6078c-1111-40db-3333-2f91eb2202b0",
+        "playerName": "Sarah"
+    },
+    {
+        "id": "1bf8af28-4ef8-4c0f-b969-66756b052da9",
+        "playerName": "Ella"
+    },
+    {
+        "id": "283c04e1-2a8a-410a-9a55-4d0dcae2d541",
+        "playerName": "Molly"
+    },
+    {
+        "id": "5982bd3f-b185-4f4e-936e-d400b0eb5f4c",
+        "playerName": "Eve"
+    },
+    {
+        "id": "7e150fd7-db5d-4a86-8f13-dd5e8469dc6a",
+        "playerName": "Josh"
+    },
+    {
+        "id": "1bf8af28-4ef8-4c0f-9999-66756b052da9",
+        "playerName": "Nathan"
+    },
+    {
+        "id": "5ea8e7de-74b7-401f-9224-f96ea34583c1",
+        "playerName": "Belle"
+    },
+    {
+        "id": "3e4713ea-07f7-4db0-b3a3-32efa27dc6fa",
+        "playerName": "Ziggy"
+    },
+    {
+        "id": "b80b35fd-1c5d-4751-baab-cb774621475c",
+        "playerName": "Kian"
+    },
+    {
+        "id": "2bf0eb4d-9e1b-44f4-89d2-efe36c76fab5",
+        "playerName": "Alex"
+    },
+    {
+        "id": "6a32f58d-b062-4820-98b2-25e9e5aadb77",
+        "playerName": "Lucy"
+    },
+    {
+        "id": "22f703d8-555d-4ef8-ba16-53f23e606d80",
+        "playerName": "Wham Sam"
+    },
+    {
+        "id": "a289cf76-2b81-46f1-af2a-794136b9736f",
+        "playerName": "Tom J"
+    },
+    {
+        "id": "15b50cc4-a366-4676-b79a-f2d360b03d1d",
+        "playerName": "Neel"
+    }
+]
 export default playersJSON;
--- src/storage/teamsJSON.ts
+++ src/storage/teamsJSON.ts
@@ -1,4 +1,47 @@
-const teamsJSON = [
+const teamsJSON = 
+[
+    {
+        "id": "f92c90ed-8d72-4f1d-9508-261c03e78666",
+        "name": "Joolly",
+        "members": [
+            {
+                "id": "7e150fd7-db5d-4a86-8f13-dd5e8469dc6a",
+                "playerName": "Josh"
+            },
+            {
+                "id": "283c04e1-2a8a-410a-9a55-4d0dcae2d541",
+                "playerName": "Molly"
+            },
+        ]
+    },
+    {
+        "id": "b6be8597-4ea9-4852-abb3-055c71874837",
+        "name": "Aura",
+        "members": [
+            {
+                "id": "1dc6078c-6488-40db-b74e-2f91eb2202b0",
+                "playerName": "Audge"
+            },
+            {
+                "id": "1bf8af28-4ef8-4c0f-b969-66756b052da9",
+                "playerName": "Ezra"
+            },
+        ]
+    },
+    {
+        "id": "27e634e1-f1ac-47d5-a055-83084cd89512",
+        "name": "Chella",
+        "members": [
+            {
+                "id": "1dc6078c-6488-40db-3333-2f91eb2202b0",
+                "playerName": "Chris"
+            },
+            {
+                "id": "1bf8af28-4ef8-4c0f-b969-66756b052da9",
+                "playerName": "Ella"
+            }
+        ]
+    },
     {
         "id": "f5776308-a446-4ead-3453245-11111",
         "name": "Sahan",
@@ -14,102 +57,74 @@ const teamsJSON = [
         ]
     },
     {
-        "id": "f5776308-a446-4ead-a454-613a3262a9dd",
-        "name": "Aura",
+        "id": "0190eeaa-5899-4bea-a4f8-3b965db91762",
+        "name": "Beve",
         "members": [
             {
-                "id": "1dc6078c-6488-40db-b74e-2f91eb2202b0",
-                "playerName": "Audge"
+                "id": "5ea8e7de-74b7-401f-9224-f96ea34583c1",
+                "playerName": "Belle"
             },
             {
-                "id": "1bf8af28-4ef8-4c0f-b969-66756b052da9",
-                "playerName": "Ezra"
-            }
+                "id": "5982bd3f-b185-4f4e-936e-d400b0eb5f4c",
+                "playerName": "Eve"
+            },
         ]
     },
     {
-        "id": "f5776000-a446-4ead-3453245-2345",
-        "name": "Toan",
-        "members": [
-            {
-                "id": "1dc6078c-9449-40db-3333-2f91eb2202b0",
-                "playerName": "Tom J"
-            },
-            {
-                "id": "1bf8af28-fgds-4c0f-b969-66756b052da9",
-                "playerName": "Kian"
-            }
-        ]
-    },
-    {
-        "id": "f5776308-a446-4ead-3453245-fsaef",
-        "name": "Neex",
-        "members": [
-            {
-                "id": "1dc6078c-6488-gsdf-gsdf-2f91eb2202b0",
-                "playerName": "Neel"
-            },
-            {
-                "id": "1bf8af28-4ef8-4c0f-gsrdf-66756b052da9",
-                "playerName": "Alex"
-            }
-        ]
-    },
-    {
-        "id": "f5776308-a446-4ead-3453245-2345",
-        "name": "Chla",
-        "members": [
-            {
-                "id": "1dc6078c-6488-40db-3333-2f91eb2202b0",
-                "playerName": "Chris"
-            },
-            {
-                "id": "1bf8af28-4ef8-4c0f-b969-66756b052da9",
-                "playerName": "Ella"
-            }
-        ]
-    },
-    {
-        "id": "0d1b72b7-4b8e-4ee7-b811-1e801c4d9a47",
-        "name": "Joolly",
-        "members": [
-            {
-                "id": "7e150fd7-db5d-4a86-8f13-dd5e8469dc6a",
-                "playerName": "Josh"
-            },
-            {
-                "id": "283c04e1-2a8a-410a-9a55-4d0dcae2d541",
-                "playerName": "Molly"
-            }
-        ]
-    },
-    {
-        "id": "f5776308-a446-2223-3453245-2345",
-        "name": "Zigcy",
-        "members": [
-            {
-                "id": "1dc6078c-6488-40db-3333-2f91eb2202b0",
-                "playerName": "Wham Sam"
-            },
-            {
-                "id": "1bf8af28-4ef8-4c0f-4421-66756b052da9",
-                "playerName": "Cooper"
-            }
-        ]
-    },
-    {
-        "id": "f5776308-2222-4ead-3453245-2345",
+        "id": "d4560dec-ebc4-492e-b8ee-5e1699757a44",
         "name": "Whamper",
         "members": [
             {
-                "id": "1dc6078c-6488-40db-3333-2f91eb2202b0",
+                "id": "22f703d8-555d-4ef8-ba16-53f23e606d80",
                 "playerName": "Wham Sam"
             },
             {
                 "id": "1bf8af28-4ef8-4c0f-4421-66756b052da9",
                 "playerName": "Cooper"
-            }
+            },
         ]
-    }
+    },
+    {
+        "id": "0aee4065-1c6b-4082-96b1-f7536f9a6126",
+        "name": "Zigcy",
+        "members": [
+            {
+                "id": "3e4713ea-07f7-4db0-b3a3-32efa27dc6fa",
+                "playerName": "Ziggy"
+            },
+            {
+                "id": "6a32f58d-b062-4820-98b2-25e9e5aadb77",
+                "playerName": "Lucy"
+            },
+        ]
+    },
+    {
+        "id": "5438586e-9029-460b-9199-6084e896941d",
+        "name": "Toan",
+        "members": [
+            {
+                "id": "a289cf76-2b81-46f1-af2a-794136b9736f",
+                "playerName": "Tom J"
+            },
+            {
+                "id": "b80b35fd-1c5d-4751-baab-cb774621475c",
+                "playerName": "Kian"
+            },
+        ]
+    },
+    {
+        "id": "27e634e1-f1ac-47d5-a055-83084cd89512",
+        "name": "Neex",
+        "members": [
+            {
+                "id": "15b50cc4-a366-4676-b79a-f2d360b03d1d",
+                "playerName": "Neel"
+            },
+            {
+                "id": "2bf0eb4d-9e1b-44f4-89d2-efe36c76fab5",
+                "playerName": "Alex"
+            },
+        ]
+    },
 ]
   export default teamsJSON;
